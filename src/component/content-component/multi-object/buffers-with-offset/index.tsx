240d895533a36f4fc9cf1d4a9606c1d505bfb0c0
julianlyzhu <julianlyzhu@tencent.com>
2023-05-22 22:34:24 +0800

    feat: 🎸 完成buffer + offset绑定资源方式

--- src/component/content-component/multi-object/buffers-with-offset/index.tsx
+++ src/component/content-component/multi-object/buffers-with-offset/index.tsx
@@ -6,8 +6,8 @@ import { RenderMultiObjectBufferWithOffsetService } from './render-multi-objects
 export default function MultiObjectBufferWithOffset() {
     useEffect(() => {
         const canvasDom = document.getElementById('render-multi-objects-buffer-with-offset')?.querySelector('canvas');
-        const renderCubeService = new RenderMultiObjectBufferWithOffsetService();
-        // renderCubeService.init(canvasDom as HTMLCanvasElement);
+        const service = new RenderMultiObjectBufferWithOffsetService();
+        service.init(canvasDom as HTMLCanvasElement);
     }, []);
     return (
         <div id="render-multi-objects-buffer-with-offset" >
